7fa76c0f83087f6a9e8b3b39c66ed839b0d28311
rymaspic <rymaspic@outlook.com>
2020-04-27 18:12:12 -0400

    update: added the QR code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Demo from './Demo';
 import * as serviceWorker from './serviceWorker';
+import QRCode from 'qrcode.react';
+import {Button} from 'antd';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+
+ReactDOM.render(
+	<div className="App">
+		<div class="Headers">
+			<h1>Covid-19 Pass</h1> 
+    		<QRCode value='https://twitter.com/realDonaldTrump/'
+    			size={150} // size
+    			fgColor="#00FF00" // QRcode color
+    		/>
+    		<Button type="primary" htmlType="update">
+          		Update QR Code
+        	</Button>
+		</div>
+
+		<div class="Form">
+			<Demo />
+		</div>
+    </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
