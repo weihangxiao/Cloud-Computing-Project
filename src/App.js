15bb6e9896aaf3f8e0764000bcdaba086a061a76
rymaspic <rymaspic@outlook.com>
2020-05-14 12:14:24 +0800

    Update: modified App.js to be a simple main page with the QR Code as the default

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import config from './aws-exports' // new
-import Amplify from 'aws-amplify' // new
+// import config from './aws-exports' // new
+// import Amplify from 'aws-amplify' // new
 import HealthForm from './components/HealthForm';
 import Code from './components/Code';
-import ChatBox from './components/ChatBox';
+// import ChatBox from './components/ChatBox';
 import './css/App.css'
-import { LioWebRTC } from 'react-liowebrtc';
+// import { LioWebRTC } from 'react-liowebrtc';
 import { Navbar, Form, Nav, FormControl } from 'react-bootstrap';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-Amplify.configure(config) // new
+// Amplify.configure(config) // new
 
 
 class App extends Component {
@@ -64,13 +64,25 @@ class App extends Component {
                 </Navbar>
                 <br></br>
                 <Router>
-                    <Route path="/input" render={props =>
+                    <div className = "link">
+                      <Link to="/"> Code </Link> //default main page is the Qr Code that we could show to others
+                      <Link to="/input"> Input </Link>
+                      <Link to="/chatbot"> Chatbot </Link>
+                    </div>
+                    <Switch>
+                      <Route exact path="/">
+                          <Code />
+                      </Route>
+                      <Route path="/input">
+                          <HealthForm />
+                      </Route>
+                      <Route path="/chatbot">
                         <div>
-                            <Code />
-                            <HealthForm />
+                            TODO: fill the chatbot page
                         </div>}
-                    />
-                    <Route path="/chatbot" render={props =>
+                      </Route>
+                    </Switch>
+                    {/*<Route path="/chatbot" render={props =>
                         <div>
                             <LioWebRTC
                                 options={options}
@@ -84,7 +96,7 @@ class App extends Component {
                                 />
                             </LioWebRTC>
                         </div>}
-                    />
+                    />*/}
                 </Router>
             </div >
         );
